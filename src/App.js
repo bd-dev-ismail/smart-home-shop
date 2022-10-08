7864fe2b1840d92691621a096207f077734e5124
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-09 02:18:53 +0600

    start project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Main from './layout/Main';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Cart from './components/Cart';
+import About from './components/About';
+const router = createBrowserRouter([
+  {path: '/',element:  <Main/>, children: [
+    {path: '/', element: <Home></Home>},
+    {path: '/home', element: <Home></Home>},
+    {path: '/shop', element: <Shop></Shop>},
+    {path: '/cart', element: <Cart></Cart>},
+    {path: '/about', element: <About></About>},
+  ]}
+])
 function App() {
-  return (
-    <div className='flex min-h-screen justify-center items-center'>
-      <h1 className='text-5xl'>Welcome to Smart Home Starter Template</h1>
-    </div>
+  return (<>
+    <RouterProvider router={router}></RouterProvider>
+  </>
   )
 }
 
